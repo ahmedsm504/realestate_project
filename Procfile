f2e5c59ad8d075b22c3598a93b63a1bdca306a0b
ahmedsm504 <ahmedsm504@gmail.com>
2025-08-22 01:55:21 +0300

    prepare project for Railway deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn my_real_estate_project.wsgi
--- my_real_estate_project/settings.py
+++ my_real_estate_project/settings.py
@@ -109,12 +109,23 @@ WSGI_APPLICATION = 'my_real_estate_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+if os.getenv("DATABASE_URL"):  # لو شغال على Railway
+    DATABASES = {
+        "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
     }
-}
+else:  # لو شغال محلي
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
