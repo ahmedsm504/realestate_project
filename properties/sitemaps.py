f21b407a0a2d11aad2562c4890864cf34977c714
ahmedsm504 <ahmedsm504@gmail.com>
2025-08-25 02:27:13 +0300

    Fix allauth settings and port configuration

--- properties/sitemaps.py
+++ properties/sitemaps.py
@@ -1,14 +1,12 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 from .models import Property
-from datetime import datetime
 
 class PropertySitemap(Sitemap):
     changefreq = "daily"
     priority = 0.9
 
     def items(self):
-        # فقط العقارات المنشورة
         return Property.objects.filter(is_published=True)
 
     def lastmod(self, obj):
@@ -17,12 +15,15 @@ class PropertySitemap(Sitemap):
     def location(self, obj):
         return obj.get_absolute_url()
 
+    # نحدد البروتوكول والدومين الأساسي
+    protocol = "https"
+
 class StaticViewSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.7
+    protocol = "https"
 
     def items(self):
-        # هنا أضف جميع الصفحات الثابتة المهمة في موقعك
         return [
             'home',
             'properties:property_list',
