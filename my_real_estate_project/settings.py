3746e07a5f98aa5a09e3aebf04582643f53db2a1
ahmedsm504 <ahmedsm504@gmail.com>
2025-08-26 18:51:14 +0300

    Fix allauth settings and port configuration

--- my_real_estate_project/settings.py
+++ my_real_estate_project/settings.py
@@ -221,16 +221,3 @@ CLOUDINARY_STORAGE = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-from cloudinary.utils import cloudinary_url
-
-
-# Upload an image
-upload_result = cloudinary.uploader.upload("https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
-                                           public_id="shoes")
-
-# Optimize delivery by resizing and applying auto-format and auto-quality
-optimize_url, _ = cloudinary_url("shoes", fetch_format="auto", quality="auto")
-
-# Transform the image: auto-crop to square aspect_ratio
-auto_crop_url, _ = cloudinary_url("shoes", width=500, height=500, crop="auto", gravity="auto")
